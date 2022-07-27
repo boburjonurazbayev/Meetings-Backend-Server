5159a025126a384899206b779905c60023c9220f
boburjonurazbayev <bobur7850@gmail.com>
2022-07-28 03:48:02 +0500

    Create mock database

---
+++ setup/mockdata.sql
@@ -0,0 +1,68 @@
+---------------------------------Admins data
+
+INSERT INTO 
+admins 
+    (admin_name, admin_password) 
+VALUES 
+    ('Boburjon', '1111');
+
+
+---------------------------------Events data 
+
+INSERT INTO 
+events 
+    (
+        event_category,
+        event_sub_category,
+        event_date,
+        event_time,
+        event_type,
+        event_link,
+        event_title,
+        event_description,
+        event_text,
+        event_photos,
+        organiser_type, 
+        organiser_name, 
+        organiser_profession, 
+        contact, 
+        organiser_company
+    ) 
+VALUES 
+    (
+        'Information Technologies', 
+        'Java developer',
+        '2022-02-22',
+        '13:00',
+        'online',
+        'https://www.youtube.com/liver24',
+        'Alisher Isaevdan biznes va IT bo`yicha master klass',
+
+        'Najot Ta`lim jamoasi o`z o`quvchilari uchun manfaatli 
+        bo`lgan musobaqalarni tashkil etishda davom etadi. 
+        Biz bu gal markazimizdagi uch soha vakillari, ya`ni 
+        UX/UI dizayner, frontend va backend dasturchilarni 
+        "bir dasturxon atrofida" to`plashga qaror qildik.',
+
+        'Najot Ta`lim jamoasi o`z o`quvchilari uchun manfaatli 
+        bo`lgan musobaqalarni tashkil etishda davom etadi. 
+        Biz bu gal markazimizdagi uch soha vakillari, ya`ni UX/UI 
+        dizayner, frontend va backend dasturchilarni "bir dasturxon 
+        atrofida" to`plashga qaror qildik.
+        Kuni kecha bo`lib o`tgan uchrashuvda to`rt kishidan iborat 
+        8 ta guruh tashkil qilinib, ularga ikki hafta muddat ichida 
+        Toshkent shahridagi har qanday onlayn va oflayn tadbirlar 
+        to`g`risida e`lonli ma`lumot beruvchi uch bosqichli veb 
+        sahifa tayyorlash vazifasi topshirildi.
+        Demak, roppa-rosa 2 haftadan keyin ishtirokchilarning 
+        qilgan ishlari chetdan kelgan mehmonlar tomonidan xolis 
+        baholanib, dastlabki uchta o`rin egalari qimmatbaho 
+        sovg`alar bilan taqdirlanadi. Biz barchaga omad tilab qolamiz.',
+        
+        '{"biznesvait.png", "itkelajagimiz.png"}',
+        'yuridik',
+        'Abbos Janizakov', 
+        'Product designer', 
+        '{"998998455055", "998330033033"}', 
+        'Najot Ta`lim MCHJ'
+    );
---
+++ setup/model.sql
@@ -0,0 +1,41 @@
+---------------------------------------------------------Create database pressa
+
+DROP DATABASE IF EXISTS pressa;
+CREATE DATABASE pressa;
+\c pressa;
+
+----------------------------------------------------------Events table
+
+create type verify as enum('unverified', 'verified', 'cancelled' );
+create type eventType as enum('online', 'offline');
+create type organizerType as enum('jismoniy', 'yuridik');
+
+DROP TABLE IF EXISTS events;
+CREATE TABLE events(
+    event_id serial primary key,
+    event_category varchar(60),
+    event_sub_category varchar(60),
+    event_date date not null,
+    event_time time not null,
+    event_type eventType not null,
+    event_link varchar(60),
+    event_title varchar(100) not null,
+    event_description text not null,
+    event_text text,
+    event_photos text [],
+    event_verify verify default 'unverified' not null, 
+    organiser_type organizerType not null,
+    organiser_company varchar(60),
+    organiser_name varchar(60) not null,
+    organiser_profession varchar(60) not null,
+    contact text []
+);
+
+---------------------------------------------------------------Admins table
+
+DROP TABLE IF EXISTS admins;
+CREATE TABLE admins(
+    admin_id serial primary key,
+    admin_name varchar(64) not null,
+    admin_password varchar(60) not null
+);
